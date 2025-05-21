0b726694fda1ba76365b8bfba99332e9008755a9
Nazrina <nazrinakhan88@gmail.com>
2025-05-20 20:59:27 -0400

    helm

--- module/eks/infra-helm.tf
+++ module/eks/infra-helm.tf
@@ -1,18 +1,16 @@
 resource "null_resource" "kube-config" {
   depends_on = [aws_eks_node_group.main]
 
-
   provisioner "local-exec" {
-    command =<<EOF
+    command = <<EOF
 aws eks update-kubeconfig --name ${var.env}-eks
 kubectl apply -f /opt/vault-token.yaml
 EOF
   }
 }
 
-## External Secrets
+## External Secrets Helm Chart
 resource "helm_release" "external-secrets" {
-
   depends_on = [null_resource.kube-config]
 
   name       = "external-secrets"
@@ -22,11 +20,21 @@ resource "helm_release" "external-secrets" {
   wait       = true
 }
 
+## Create ClusterSecretStore after CRD becomes available
 resource "null_resource" "external-secrets-store" {
   depends_on = [helm_release.external-secrets]
 
   provisioner "local-exec" {
-    command =<<EOF
+    command = <<EOF
+# Wait until ClusterSecretStore CRD is registered
+echo "Waiting for ClusterSecretStore CRD to be ready..."
+for i in {1..20}; do
+  kubectl get crd clustersecretstores.external-secrets.io && break
+  echo "Retry ($i)..."
+  sleep 5
+done
+
+# Apply ClusterSecretStore
 kubectl apply -f - <<EOK
 apiVersion: external-secrets.io/v1beta1
 kind: ClusterSecretStore
