4289d658b5832e54bdce738e7f569b10547393fd
Nazrina <nazrinakhan88@gmail.com>
2025-05-20 21:07:42 -0400

    helm

--- module/eks/infra-helm.tf
+++ module/eks/infra-helm.tf
@@ -1,3 +1,24 @@
+resource "null_resource" "kube-config" {
+  depends_on = [aws_eks_node_group.main]
+
+  provisioner "local-exec" {
+    command = <<EOF
+aws eks update-kubeconfig --name ${var.env}-eks
+kubectl apply -f /opt/vault-token.yaml
+EOF
+  }
+}
+
+resource "helm_release" "external-secrets" {
+  depends_on = [null_resource.kube-config]
+
+  name       = "external-secrets"
+  repository = "https://charts.external-secrets.io"
+  chart      = "external-secrets"
+  namespace  = "kube-system"
+  wait       = true
+}
+
 resource "null_resource" "external-secrets-store" {
   depends_on = [helm_release.external-secrets]
 
