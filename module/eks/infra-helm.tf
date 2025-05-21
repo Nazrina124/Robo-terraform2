236e26e20813fd04033aebb1cc815868f29ddc5b
Nazrina <nazrinakhan88@gmail.com>
2025-05-20 20:32:03 -0400

    helm

--- module/eks/infra-helm.tf
+++ module/eks/infra-helm.tf
@@ -35,7 +35,7 @@ metadata:
 spec:
   provider:
     vault:
-      server: "http://vault-internal.naifah.online:8200/"
+      server: "http://vault.naifah.online:8200/"
       path: "roboshop-${var.env}"
       version: "v2"
       auth:
